2664ce057546c56847b6ccbfebc42a6fc738685c
Jordan Williams <jordanw0412@gmail.com>
2024-01-12 02:48:48 -0500

    compiled index.ts

--- dist/index.js
+++ dist/index.js
@@ -2,5 +2,15 @@
 let number1 = 1;
 number1 = 3;
 let string1 = "test";
-console.log(string1);
+let string2 = "test";
+string2 = "world";
+let anyVariable;
+anyVariable = "a string";
+anyVariable = 4;
+let numbers = [1, 2, 3];
+let numbers2 = [1, 2, 3];
+let strings = ["hello", "world"];
+let anyArray = [];
+let numbers3 = [];
+numbers3.forEach(n => n.toFixed);
 //# sourceMappingURL=index.js.map
